4f3732b29c5f3e6ec3108089451209fc10d201ab
Fulvio Casali <fulviocasali@gmail.com>
2015-06-08 16:38:41 -0700

    Add a new text field after first checkbox, with special formatting.

--- sibs/sibshops/resources/main.js
+++ sibs/sibshops/resources/main.js
@@ -14,6 +14,7 @@ jQuery(function($){
   $('form #formfield-form-widgets-sponsor_phone label').addClass('field_online');
   $('form #formfield-form-widgets-sponsor_email label').addClass('field_online');
   $('form #formfield-form-widgets-agency_site label').addClass('field_online');
+  $('form #formfield-form-widgets-facilitator_training label').addClass('field_plain');
 
   $('form #formfield-form-widgets-bool_standards label').html(
       'We have reviewed and endorsed the <a target="_blank" href="https://www.siblingsupport.org/about-sibshops/for-sibshop-providers/sibshops-standards-of-practice">Sibshop Standards of Practice</a>'
@@ -23,5 +24,8 @@ jQuery(function($){
       'We have joined <a target="_blank" href="https://www.siblingsupport.org/about-sibshops/for-sibshop-providers/sibgroup-a-yahoogroup-for-sibshop-providers">SibGroup</a>'
    );
 
+  $('form #formfield-form-widgets-facilitator_training label').html(
+      "Please read Standard 10 of the Sibshops Standards of Practice and describe your agency's efforts to obtain training on the Sibshop model. Specifically, please note whether your Sibshop facilitators are first-generation or second-generation facilitators and <b>where and when</b> they received their training. <b>Registering with second-generation facilitators requires prior approval from the Sibling Support Project.</b>"
+   );
 });
 
