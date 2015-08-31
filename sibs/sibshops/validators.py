e97e27aff9e2edf10cb4052ab4d01733d272c144
Fulvio Casali <fulviocasali@gmail.com>
2015-08-31 12:10:01 -0700

    The parent does not need the lower.

--- sibs/sibshops/validators.py
+++ sibs/sibshops/validators.py
@@ -11,7 +11,7 @@ class EmailValidator(SimpleFieldValidator):
     regex = r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?"
 
     def validate(self, value):
-        super(EmailValidator, self).validate(value.lower())
+        super(EmailValidator, self).validate(value)
         if value:
             if not re.match(self.regex, value.lower()) or value.endswith('.'):
                 raise Invalid(_("Not a valid e-mail address"))
